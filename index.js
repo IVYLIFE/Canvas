b4bd0ed599c6820b75b0723f310b28ef2d2def43
Himanshu <thisivylife01@gmail.com>
2023-10-01 00:13:50 +0530

    Added Some Canvas Features

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const shapeContainer = document.getElementById('shapes');
 const dropdown = document.querySelector('.dropdown');
 let shape = document.getElementById('choosedShape');
 let strokeSize = document.getElementById('strokeSize');
+let selectedShape = '';
 strokeSize.defaultValue = "10";
 
 
@@ -18,8 +19,6 @@ let currentColor = '#000000'
 
 localStorage.setItem('#dae2be', 'Plain');
 localStorage.setItem('#76d173', 'Grassy');
-console.log(palatte)
-
 
 
 window.onload = () => {
@@ -46,12 +45,49 @@ window.onload = () => {
 }
 
 
-
 const edit = (e) => {
     target = this.event.target;
+    console.log(target);
     target.contentEditable = true;
     target.focus();
     target.classList.add('active');
+
+    let key = target.previousElementSibling.style.backgroundColor;
+    key  = key.replace("rgb(", "");
+    key  = key.replace(")", "");
+
+    let rgb = key.split(",");
+
+    for (value in rgb) {   
+        rgb[value] = parseInt(rgb[value]);
+    }
+
+    let hexR = parseInt(rgb[0]).toString(16);
+    let hexG = parseInt(rgb[1]).toString(16);
+    let hexB = parseInt(rgb[2]).toString(16);
+
+
+    if (hexR.length == 1) {
+        hexR = "0" + hexR;
+    }
+
+    if (hexG.length == 1) {
+        hexG = "0" + hexG;
+    }
+
+    if (hexB.length == 1) {
+        hexB = "0" + hexB;
+    }
+
+    key = "#" + hexR + hexG + hexB;
+    console.log(key);
+
+    target.addEventListener('blur', () => {
+        localStorage.setItem(key, target.innerHTML);}
+    )
+
+
+
 }
 
 const changeColor = () => {
@@ -85,14 +121,13 @@ decreaseBtn.addEventListener('click', () => {
 });
 
 dropdown.addEventListener('click', () => {
-    console.log('clicked');
     shapeContainer.classList.toggle('active');
 });
 
 document.querySelectorAll('#shapeList li').forEach((item) => {
     item.addEventListener('click', () => {
-        console.log(item.id);
         shape.innerHTML = item.id.charAt(0).toUpperCase() + item.id.slice(1);
+        selectedShape = item.id;
         shapeContainer.classList.remove('active');
     });
 });
@@ -114,6 +149,43 @@ colorPicker.onchange = function () {
     descriptions.innerHTML += newColorDesc;
     palatte.innerHTML += newColor;
 }
+
+
+// currentColor, strokeSize, selectedShape
+
+let canvas = document.querySelector('canvas');
+let ctx = canvas.getContext('2d');
+
+
+let painting = false;
+let width = canvas.innerWidth;
+let height = canvas.innerHeight;
+
+console.log( currentColor, strokeSize.value, selectedShape);
+
+
+function drawCircle(x, y, radius, color) {
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI * 2, true);
+    ctx.fillStyle = color;   
+    ctx.fill();
+}
+
+function drawLine(x1, y1, x2, y2, color) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = strokeSize.value;
+    ctx.stroke();
+    ctx.closePath();
+}
+
+function drawRect(x, y, width, height, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, width, height);
+}
+
 
 
 
